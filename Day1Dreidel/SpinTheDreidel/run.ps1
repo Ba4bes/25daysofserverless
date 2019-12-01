550c53ee09b622bd51a5876ab38a449b58acab70
Barbara Forbes <ba@4bes.nl>
2019-12-01 12:15:53 +0100

    changed result to json-format

--- Day1Dreidel/SpinTheDreidel/run.ps1
+++ Day1Dreidel/SpinTheDreidel/run.ps1
@@ -6,17 +6,28 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+# List the possible outcomeoptions in an array
 $OutcomeOptions = @(
     " נ"
     " ג"
     " ה"
 )
 
-$Return = $OutcomeOptions | Get-Random -Count 1
+# Create an object that will be returned (as a json)
+$Return = New-Object PSObject –Property @{
+    "Success" = '$true'
+    "Message" = "The dreidel is done spinning"
+}
+
+# Get a random value from the outcomeoptions
+$Result = $OutcomeOptions | Get-Random -Count 1
+
+# add thatvalues to the return
+$Return | Add-Member -MemberType NoteProperty -Name "data" -Value @{"Result" = $Result }
 Write-Host "Return: $Return"
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
-        Body       = $Return
-})
+        Body       = ($Return | ConvertTo-Json)
+    })
