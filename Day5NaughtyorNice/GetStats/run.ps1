56b20a827d4177e2a69b42b63dae0be337b3a35f
Barbara <ba@4bes.nl>
2020-01-01 16:43:30 +0100

    Code Cleanup Day 5

--- Day5NaughtyorNice/GetStats/run.ps1
+++ Day5NaughtyorNice/GetStats/run.ps1
@@ -1,10 +1,30 @@
 using namespace System.Net
 
+<#
+.SYNOPSIS
+    This Azure Function App gets set messages, translates them and analyzes the possitivity
+.DESCRIPTION
+    The messages are collected from an API call. First the languages are detected.
+    After that the message is translated and checked for positivity.
+    The messages are collected for each child to find out if they were nice or naughty
+.EXAMPLE
+    visit "https://4besday5.azurewebsites.net/api/GetStats"
+.INPUTS
+    Needs to be called in a webbrowser
+.OUTPUTS
+    A HTML based website with a table with information on the children
+.NOTES
+    This is an Azure Function App, originally made for 25DaysofServerless.com
+    Made by Barbara Forbes
+    @Ba4bes
+    4bes.nl
+#>
+
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-
-$Header = @{
+# First create the headers for the API requests. The keys are collected from a KeyVault Account.
+$TranslateHeader = @{
     "Ocp-Apim-Subscription-Key" = (Get-AzKeyVaultSecret -VaultName forbesDay5 -Name translateKey).SecretValueText
     "Content-Type"              = "application/json"
 }
@@ -12,6 +32,8 @@ $analyticsheader = @{
     "Ocp-Apim-Subscription-Key" = (Get-AzKeyVaultSecret -VaultName forbesDay5 -Name AnalyticsKey).SecretValueText
     "Content-Type"              = "application/json"
 }
+
+# The output will be a HTTP page with a table. Here we create a header for that page
 $HTMLHeader = @"
 <style>
 BODY {font-family:verdana;}
@@ -21,62 +43,93 @@ TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black; p
 </style>
 "@
 
-# $token = Invoke-RestMethod -Method POST -header $header -uri 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+# Create an empty array for the results
 $Results = @()
 
-$letters = Invoke-RestMethod -Method GET https://christmaswishes.azurewebsites.net/api/Wishes
-$Children = $letters | Select-Object -Unique -Property Who
+# The example messages are collected from an API.
+# As there is more than one message for each kid, the childrens names are collected to collect all messages from each kid.
+$Letters = Invoke-RestMethod -Method GET -URI https://christmaswishes.azurewebsites.net/api/Wishes
+$Children = $Letters | Select-Object -Unique -Property Who
+
 foreach ($Child in $Children) {
+    # Create an array to collect the different scored for this child
     $AllScores = @()
-    Write-Output $child
+    Write-Output $Child
+    # Collected every letter written by this child and go throught them
     $Messages = $Letters | Where-Object { $_.Who -eq $Child.Who }
     foreach ($Message in $Messages) {
-        if ([string]::IsNullOrEmpty($Message.message)) { Write-output "no message" }
+        if ([string]::IsNullOrEmpty($Message.message)) {
+            Write-output "no message"
+        }
         else {
+            # Create a body and change it tot the correct format for translation to English
             $body = @{
                 Text = $Message.message
             }
-            $bodyjson = "[$($body | ConvertTo-Json)]"  #| foreach { [System.Text.RegularExpressions.Regex]::Unescape($_) } )]"
+            $bodyjson = "[$($body | ConvertTo-Json)]"
+            # Changes encoding to handle non-standard characters
             $jsondata = ([System.Text.Encoding]::UTF8.GetBytes($bodyjson))
-            $Language = Invoke-RestMethod -Method POST -uri "https://api.cognitive.microsofttranslator.com/detect?api-version=3.0" -Headers $header -Body $jsondata
+            # Detect the language of the message
+            $URI = "https://api.cognitive.microsofttranslator.com/detect?api-version=3.0"
+            $Language = Invoke-RestMethod -Method POST -uri $URI -Headers $TranslateHeader -Body $jsondata
 
+            # Translate the message to English.
+            $URI = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=$($Language.Language)&to=en"
+            $Result = Invoke-RestMethod -Method POST -uri $URI -headers $TranslateHeader -body $jsondata
+            Write-Output $Result.translations.text
 
-            $Result = Invoke-RestMethod -Method POST "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=$($Language.Language)&to=en" -header $header -body $jsondata
-            $result.translations.text
-
-            $abody = @{
+            # Create the body for the Analytics API call
+            $AnyalyticsBody = @{
                 "language" = "en"
                 "id"       = "1"
-                "text"     = "$($result.translations.text)"
+                "text"     = "$($Result.translations.text)"
             }
-            $documents = @{"Documents" = @($aBody) } | ConvertTo-Json
-            $Score = Invoke-RestMethod -Method POST -Body $documents -Headers $analyticsheader -uri "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment"
-            $AllScores += $score.documents.score
-            $AvScore = ($AllScores | Measure-Object -Average).Average
-            if ($AvScore -gt 0.5) {
-                $NaughtyorNice = "Nice"
-            }
-            elseif ($AvScore -lt 0.5) {
-                $NaughtyorNice = "Naughty"
-            }
-            else {
-                $NaughtyorNice = "Neutral"
-            }
-            $ChildObject = [pscustomobject]@{
-                Name          = $Child.who
-                Score         = $AvScore
-                NaughtyorNice = $NaughtyorNice
-            }
+            $AnalyticsJson = @{"Documents" = @($AnyalyticsBody) } | ConvertTo-Json
+
+            # Get the score from the API
+            $URI = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment"
+            $Score = Invoke-RestMethod -Method POST -uri $URI -Headers $AnalyticsHeader -Body $AnalyticsJson
+            # Collect the API call and add it to the other scores this child has generated
+            $AllScores += $Score.documents.score
         }
     }
+    # Get the average score from all the different messages and give back a result based on that
+    $AvScore = ($AllScores | Measure-Object -Average).Average
+    if ([string]::IsNullOrEmpty($AllScores)) {
+        Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode  = [HttpStatusCode]::BadRequest
+                ContentType = "text/html"
+                Body        = "Could not collect scores"
+            })
+        exit
+    }
+    elseif ($AvScore -gt 0.5) {
+        $NaughtyorNice = "Nice"
+    }
+    elseif ($AvScore -lt 0.5) {
+        $NaughtyorNice = "Naughty"
+    }
+    else {
+        $NaughtyorNice = "Neutral"
+    }
+    # Create an object with all information
+    $ChildObject = [pscustomobject]@{
+        Name          = $Child.who
+        Score         = $AvScore
+        NaughtyorNice = $NaughtyorNice
+    }
+
+
+    # Add the created object tot the results
     $Results += $ChildObject
 }
 
-
+# Take the results and create an output with the Header
 $HTML = $HTMLHeader + ($Results | ConvertTo-Html)
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode  = "ok"
+        StatusCode  = [HttpStatusCode]::OK
         ContentType = "text/html"
         Body        = $HTML
     })
--- Day5NaughtyorNice/Readme.md
+++ Day5NaughtyorNice/Readme.md
@@ -2,14 +2,15 @@
 
 ## Process
 
-Some new challenges for me. The first time I have used cogitive services :)
+Some new challenges for me. The first time I have used cognitive services :)
 They are called by a Function App that also used Key Vault for the API secrets.
 The output is an HTML page.
 
-At this point I have combined the messages of the children and taken an average score. 
-It does seam they are all naughty at this point, so I might change it up a bit later.
+At this point I have combined the messages of the children and taken an average score.
 
-## Call the function 
+**Update**: I have cleaned up the code. [Click here for the original submission](https://github.com/Ba4bes/25daysofserverless/tree/2c3135f1f2b1bdad8c4f2d2a8b1865815f9fc886/Day5NaughtyorNice)
+
+## Call the function
 
 The function is meant to be visited in a browser: <https://4besday5.azurewebsites.net/api/GetStats>
 
@@ -17,14 +18,14 @@ The function is meant to be visited in a browser: <https://4besday5.azurewebsite
 
 ### Naughty or Nice
 
-*Thursday 5 December 2019* 
+*Thursday 5 December 2019*
 It's freezing cold up here on the North Pole, which normally makes it the ideal place to host a server farm. But today Santa's elves are freaking out!
 
 Children all over the world write Santa letters to say what they want for Christmas. The elves had scripts running locally on the server farm to process the letters but without the missing servers this is no longer possible. Santa could translate manually, but he won't be able to get through all the letters in time!
 
 Write a serverless application that helps Santa figure out if a given child is being naughty or nice based on what they've said. You'll likely need to detect the language of the correspondence, translate it, and then perform sentiment analysis to determine whether it's naughty or nice.
 
-Have a look at the API https://aka.ms/holiday-wishes to find a sample of messages to validate whether your solution will work for Santa and his elves. 
+Have a look at the API <https://aka.ms/holiday-wishes> to find a sample of messages to validate whether your solution will work for Santa and his elves.  
 Here in Brooklyn, NY, Ezra wants to have a big holiday potluck before everyone travels home for the holidays! His tiny apartment can barely fit everyone in, but it's a cozy way to celebrate with friends. He usually uses an online spreadsheet to coordinate who's bringing what, to make sure there's varieties of food to meet all dietary needs.
 
 But the grinch stole all the servers! So Ezra can't do that this year.
@@ -33,14 +34,11 @@ Build an HTTP API that lets Ezra's friends add food dishes they want to bring to
 
 ## More information
 
-# Useful links
+### Useful links
 
+<https://blog.kloud.com.au/2018/08/28/using-azure-cognitive-services-language-text-translation-with-powershell/>
 
-
-https://blog.kloud.com.au/2018/08/28/using-azure-cognitive-services-language-text-translation-with-powershell/
-
-https://stackoverflow.com/questions/15290185/invoke-webrequest-issue-with-special-characters-in-json
-
+<https://stackoverflow.com/questions/15290185/invoke-webrequest-issue-with-special-characters-in-json>
 
  This api is running on an Azure Function App with PowerShell. Want to find out more about creating your own? Click [here]('https://4bes.nl/MSIgnite')
 
