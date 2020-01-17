095c91ea8f9ad1cb6461cede3b0a8bd5e9e1baf9
Barbara <ba@4bes.nl>
2020-01-17 09:49:12 +0100

    first improvements

--- Day7Pictures/FrontPage/run.ps1
+++ Day7Pictures/FrontPage/run.ps1
@@ -3,24 +3,26 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
-$form = @"
+
+$FunctionName = $($TriggerMetadata.FunctionName)
+
+# This is a lazy string to get some input
+$Form = @"
 <html>
 <body>
 <h1>la quema del diablo</h1>
 <form action='http://4besday7.azurewebsites.net/Result'>
-What do you want to throw away? <input type='text' name='SearchItem'><br>
+What are you looking for? <input type='text' name='SearchItem'><br>
 <input type='submit' value='Submit'>
 </form>
-
 <p>
-
 </body>
 "@
 
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = "ok"
-    ContentType = "text/html"
-    Body = $form
-})
+        StatusCode  = [HttpStatusCode]::OK
+        ContentType = "text/html"
+        Body        = $Form
+    })
