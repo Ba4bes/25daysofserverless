ea2c64e1fc3c85c9811c9c829692e1d951c5e6a5
Barbara 4bes <43912004+Ba4bes@users.noreply.github.com>
2020-01-25 19:13:53 +0100

    Cleanup day7 (#8)
    * first improvements
    
    * remove key
    
    * Cleanup Day 7

--- Day7Pictures/FrontPage/run.ps1
+++ Day7Pictures/FrontPage/run.ps1
@@ -1,26 +1,45 @@
 using namespace System.Net
-
+<#
+.SYNOPSIS
+    This Function calls a html-form where the user can give an input
+.DESCRIPTION
+    TAn HTMLform is returned to the user.
+    The input is then given to the second function called "Result"
+.INPUTS
+    a webrequest through a browser
+.OUTPUTS
+    an HTML page with a form
+.NOTES
+    This is an Azure Function App, originally made for 25DaysofServerless.com
+    Made by Barbara Forbes
+    @Ba4bes
+    4bes.nl
+#>
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
-$form = @"
+
+# Get the FunctionAppURL for the resultFunction
+Write-Host $TriggerMetadata.request.url
+$URL = $TriggerMetadata.Request.Url
+$FunctionAppUrl = $URL -replace "FrontPage", "Result"
+
+# To get the input from the user, a simple HTMLform is used. 
+$Form = @"
 <html>
 <body>
 <h1>la quema del diablo</h1>
-<form action='http://4besday7.azurewebsites.net/Result'>
-What do you want to throw away? <input type='text' name='SearchItem'><br>
+<form action='$FunctionAppUrl'>
+What are you looking for? <input type='text' name='SearchItem'><br>
 <input type='submit' value='Submit'>
 </form>
-
 <p>
-
 </body>
 "@
 
-
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = "ok"
-    ContentType = "text/html"
-    Body = $form
-})
+        StatusCode  = [HttpStatusCode]::OK
+        ContentType = "text/html"
+        Body        = $Form
+    })
